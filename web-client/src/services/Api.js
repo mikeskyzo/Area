01e4aba5de73e555ced55161c657efa4f22c8018
MinMatth-Magi <matthieu.desrues@epitech.eu>
2020-01-20 16:44:58 +0100

    [ADD] Simple register, not found bro

--- web-client/src/services/Api.js
+++ web-client/src/services/Api.js
@@ -1,7 +1,12 @@
 import axios from 'axios'
 
+// eslint-disable-next-line no-unused-vars
+const server = `http://server:8080/`;
+// eslint-disable-next-line no-unused-vars
+const local = `http://localhost:8080/`;
+
 export default () => {
   return axios.create({
-    baseURL: `http://localhost:8081/`
+    baseURL: local
   })
 }
