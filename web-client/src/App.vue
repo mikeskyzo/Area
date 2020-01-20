3b6d87251f5bf7341c2db9cc91b7a50a7b1265ae
Matthieu <desrues.matthieu@gmail.com>
2020-01-20 16:44:58 +0100

    [ADD] Simple register, not found bro

--- web-client/src/App.vue
+++ web-client/src/App.vue
@@ -1,7 +1,7 @@
 <template>
 
   <v-app>
-    <v-content>
+    <v-content fluid fill-height>
       <router-view/>
     </v-content>
   </v-app>
--- web-client/src/router/index.js
+++ web-client/src/router/index.js
@@ -2,8 +2,10 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Authentication from '../views/Authentication.vue'
+import NotFound from "../views/NotFound";
+import Dashboard from "../views/Dashboard";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
@@ -17,15 +19,25 @@ const routes = [
     component: Authentication
   },
   {
+    path: '/dashboard',
+    name: 'dashboard',
+    component: Dashboard
+  },
+  {
+    path: '/notFound',
+    name: 'notFound',
+    component: NotFound
+  },
+  {
     path: '*',
-    redirect: 'home'
+    redirect: 'notFound',
   }
-]
+];
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
-})
+});
 
 export default router
