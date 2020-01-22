d740e77188410a2f113c96b8bfc71a0f7a019fc2
Mike <mikeal.gerard@epitech.eu>
2020-01-22 10:24:38 +0100

    ADD : structure of the areas

--- server/routes/areaCreator.js
+++ server/routes/areaCreator.js
@@ -3,7 +3,6 @@ var uniqid = require('uniqid');
 
 var router = express.Router();
 
-var users = require('./users');
 var weather = require('../services/weather')
 
 router.post('/CreateArea', function(req, res, next) {
@@ -17,7 +16,7 @@ router.post('/CreateArea', function(req, res, next) {
         });
 		return;
 	}
-	users.DoesUserExist(req.body.user_id, req, res, redirectToAction);
+	global.DoesUserExist(req.body.user_id, req, res, redirectToAction);
 });
 
 function redirectToAction(req, res)
@@ -30,13 +29,37 @@ function redirectToAction(req, res)
 	switch (req.body.action) {
 		case global.Action_weather_time:
 			if (req.body.time && req.body.city) {
-				json.actionParams = {
+				json.action_params = {
 					'city' : req.body.city,
 					'time' : req.body.time
 				}
 				weather.DoesCityExist(req.body.city, req, res, json, redirectToReaction)
 				return;
 			}
+			break;
+		case global.Action_weather_change :
+			break;
+		case global.Action_imgur_new_post :
+			break;
+		case global.Action_reddit_new_post_sub :
+			break;
+		case global.Action_reddit_new_post_follower :
+			break;
+		case global.Action_reddit_new_notification :
+			break;
+		case global.Action_clock_time_place :
+			break;
+		case global.Action_steam_players_on_game :
+			break;
+		case global.Action_steam_friend_online :
+			break;
+		case global.Action_discord_new_message :
+			break;
+		case global.Action_discord_user_send_message :
+			break;
+		case global.Action_youtube_channel_sub :
+			break;
+		case global.Action_youtube_channel_views :
 			break;
 		default:
 			responseError(res, 401, 'Bad header');
@@ -51,13 +74,21 @@ function redirectToReaction(req, res, json)
 	switch (req.body.reaction) {
 		case global.Reaction_discord_send_message:
 			if (req.body.channel_id && req.body.message) {
-				json.reactionParams = {
+				json.reaction_params = {
 					'channel_id' : req.body.channel_id,
 					'message' : req.body.message
 				};
 				createAREA(req, res, json);
 				return;
 			}
+			break;
+		case global.Reaction_discord_message_reaction :
+			break;
+		case global.Reaction_imgur_upload_picture :
+			break;
+		case global.Reaction_reddit_post_a_post :
+			break;
+		case global.Reaction_reddit_post_vote :
 			break;
 		default:
 			responseError(res, 401, 'Bad header');
@@ -80,16 +111,22 @@ router.get('/GetArea', function(req, res, next) {
         });
 		return;
 	}
-    global.db.collection(global.CollectionArea).find({user_id : user_id}).toArray(function (err, result) {
+	global.DoesUserExist(user_id, req, res, getAreas);
+});
+
+function getAreas(req, res)
+{
+    global.db.collection(global.CollectionArea).find({user_id : req.body.user_id}).toArray(function (err, result) {
         if (err) {
             res.status(401);
             res.json({
                 success : false,
                 message : err.message
-            });
+			});
+			return;
 		}
 		res.json(result);
     })
-});
+}
 
 module.exports = router;
