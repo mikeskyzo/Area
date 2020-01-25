cd57c466424c9a755180cb820c4e60ae839135de
Mike <mikael.gerard@epitech.eu>
2020-01-25 22:57:12 +0100

    ADD : tokens

--- server/routes/tokens.js
+++ server/routes/tokens.js
@@ -1,19 +1,4 @@
-var express = require('express');
-var router = express.Router();
-
-router.post('/auth/:app', function(req, res) {
-	if (!req.body.user_id) {
-		res.status(401);
-		res.json({
-			success : false,
-			message : 'Bad body'
-		});
-		return;
-	}
-	global.DoesUserExist(req.body.user_id, req ,res, redirectApp);
-});
-
-function redirectApp(req, res)
+exports.newAuth = function (req, res)
 {
 	if (!req.body.access_token || !req.body.refresh_token || !req.body.expires_in) {
 		res.status(401);
@@ -42,19 +27,7 @@ function redirectApp(req, res)
 	global.saveInDb(global.CollectionToken, json, req, res, 'Token saved');
 };
 
-router.get('/auth/getServices', function(req, res) {
-	if (!req.body.user_id) {
-		res.status(401);
-		res.json({
-			success : false,
-			message : 'Bad body'
-		});
-		return;
-	}
-	global.DoesUserExist(req.body.user_id, req ,res, getServices);
-});
-
-function getServices(req, res)
+exports.getServices = function (req, res)
 {
 	global.db.collection(global.CollectionToken).find({user_id : req.body.user_id}).toArray(function (err, result) {
         if (err) {
@@ -86,6 +59,4 @@ function getServices(req, res)
 		}
 		res.json(json);
     })
-}
-
-module.exports = router;
+};
