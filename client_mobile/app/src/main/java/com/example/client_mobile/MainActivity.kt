b61ef096eb191adf0f5be636bed4b07f116a76bf
Jean <jean.bourgarel@epitech.eu>
2020-02-04 11:58:57 +0100

    trying to post data to server from mobile_client

--- client_mobile/app/src/main/java/com/example/client_mobile/MainActivity.kt
+++ client_mobile/app/src/main/java/com/example/client_mobile/MainActivity.kt
@@ -1,13 +1,14 @@
 package com.example.client_mobile
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.google.gson.GsonBuilder
 import okhttp3.*
+import okhttp3.FormBody
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.RequestBody.Companion.toRequestBody
-import org.json.JSONObject
 import java.io.IOException
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,24 +18,61 @@ class MainActivity : AppCompatActivity() {
         askForConnection()
     }
 
+
     fun askForConnection() {
 
-        val mediaType = "application/json; charset=utf-8".toMediaType()
 
-        val magie = GsonBuilder().create()
-        val jsonString = magie.toJson(Account("jean", "homo"))
-
-        val myJSONObject = "{username: \"jean\", password: \"homo\"}"
-        val requestBody = jsonString.toRequestBody(mediaType)
-
-
-        val url = "http://server:8080/createUser"
-        val request = Request.Builder().url(url).post(requestBody).build()
         val client = OkHttpClient()
+        val formBody: RequestBody = FormBody.Builder()
+            .add("username", "test")
+            .add("password", "cul")
+            .build()
+
+        val request: Request = Request.Builder()
+            .url("http://localhost:8080/createUser")
+            .post(formBody)
+            .build()
+
+        val call: Call = client.newCall(request)
+        val response = call.execute()
 
         client.newCall(request).enqueue(object: Callback {
             override fun onResponse(call: Call, response: Response) {
                 val body = response.body?.string()
+                println("response ouui oui oui")
+                println(body)
+
+            }
+            override fun onFailure(call: Call, e: IOException) {
+                println("Failed to execute request")
+            }
+        })
+
+
+
+
+/*        val mediaType = "application/json; charset=utf-8".toMediaType()
+
+        val jsonString = GsonBuilder().create().toJson(Account("jeanss", "homo"))
+        val requestBody = jsonString.toRequestBody(mediaType)
+
+        println("START")
+
+        val url = "http://localhost:8080/createUser"
+        val request = Request.Builder().url(url).post(requestBody).build()
+        val client = OkHttpClient()
+
+
+        println(jsonString)
+        println(requestBody)
+        println(request)
+        println("END")
+
+
+        client.newCall(request).enqueue(object: Callback {
+            override fun onResponse(call: Call, response: Response) {
+                val body = response.body?.string()
+                println("response ouui oui oui")
                 println(body)
                 val gson = GsonBuilder().create()
 
@@ -44,7 +82,7 @@ class MainActivity : AppCompatActivity() {
             override fun onFailure(call: Call, e: IOException) {
                 println("Failed to execute request")
             }
-        })
+        })*/
         println("slap her")
     }
 
