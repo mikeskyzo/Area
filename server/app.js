52ce3dd944e151466aa38de5456fa9a673ec6bf9
Mike <mikael.gerard@epitech.eu>
2020-02-04 09:31:27 +0100

    ADD : REACTION : discord send a message on a channel

--- server/app.js
+++ server/app.js
@@ -9,6 +9,8 @@ var routes = require('./routes/routes');
 var webhooks = require('./routes/webhooks');
 var weather = require('./services/weather')
 
+var logger = require('morgan');
+
 var utils = require('./src/utils')
 var mongoDb = require('./src/manageDb')
 
@@ -16,9 +18,11 @@ var app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(logger('dev'));
+app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json());
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
@@ -35,20 +39,11 @@ mongoDb.initDb();
 // Init all services
 weather.initWeather();
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    next(createError(404));
+app.route('/home').get(function(req, res) {
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-    // render the error page
-    res.status(err.status || 500);
-    res.json({});
+app.route('/test').get(function(req, res) {
+    res.json({ok : 'boomer'})
 });
 
 app.listen(8080, function (req, res) {
--- server/services/discord.js
+++ server/services/discord.js
@@ -1,8 +1,22 @@
-const api_client_id = '669119089033347078'
+const api_client_id = '672107018710155295'
 const api_client_secret = 'n3lnZRonjfue-nwjhxXFL_-Tmncx5pz-'
+const redirect_uri = 'http%3A%2F%2Fwww.localhost%3A8080%2Fhome'
 
+const requestOAuth2 = 'https://discordapp.com/oauth2/authorize?client_id=' & api_client_id & '&redirect_uri=' & redirect_uri & '&response_type=token&scope=identify'
 
-module.exports.send_message = function(params, user_id, result)
+const Discord = require('discord.js');
+
+exports.send_message = function(params, user_id, result, tokens)
 {
+    const Discord = require('discord.js');
+    const client = new Discord.Client();
 
+    client.login('NjcyMTA3MDE4NzEwMTU1Mjk1.Xjg51A.88cce2c9l-vqSoNRcX59MMDeOSA');
+
+    client.on('ready', () => {
+        console.log(`Logged in as ${client.user.tag}!`);
+        const channel = client.channels.get(params.channel_id);
+        if (channel)
+            channel.send(global.enhanceMessage(params.message, result));
+    });
 }
