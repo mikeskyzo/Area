2d9cfbfe510e0e18c7ba41ad2273197c3447b605
Jean <jean.bourgarel@epitech.eu>
2020-02-05 10:33:02 +0100

    mobile requests to server are working

--- client_mobile/app/src/main/java/com/example/client_mobile/Start.kt
+++ client_mobile/app/src/main/java/com/example/client_mobile/Start.kt
@@ -37,11 +37,9 @@ class Start : AppCompatActivity() {
             .build()
 
         val request: Request = Request.Builder()
-            .url("http://10.29.125.210:8080/createUser")
+            .url("http://10.29.124.224:8080/createUser")
             .post(formBody)
             .build()
-
-        //val call: Call = client.newCall(request)
 
         client.newCall(request).enqueue(object: Callback {
             override fun onResponse(call: Call, response: Response) {
@@ -55,43 +53,6 @@ class Start : AppCompatActivity() {
                 println(e)
             }
         })
-
-
-
-
-/*        val mediaType = "application/json; charset=utf-8".toMediaType()
-
-        val jsonString = GsonBuilder().create().toJson(Account("jeanss", "homo"))
-        val requestBody = jsonString.toRequestBody(mediaType)
-
-        println("START")
-
-        val url = "http://localhost:8080/createUser"
-        val request = Request.Builder().url(url).post(requestBody).build()
-        val client = OkHttpClient()
-
-
-        println(jsonString)
-        println(requestBody)
-        println(request)
-        println("END")
-
-
-        client.newCall(request).enqueue(object: Callback {
-            override fun onResponse(call: Call, response: Response) {
-                val body = response.body?.string()
-                println("response ouui oui oui")
-                println(body)
-                val gson = GsonBuilder().create()
-
-                val homeFeed = gson.fromJson(body, Account::class.java)
-                println(homeFeed)
-            }
-            override fun onFailure(call: Call, e: IOException) {
-                println("Failed to execute request")
-            }
-        })*/
-        println("punch her")
     }
 
     class Account(val username: String, val password: String)
