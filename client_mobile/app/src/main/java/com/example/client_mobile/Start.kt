305fd983068c87c2a66f72d772e9eaecdf97c233
Jean <jean.bourgarel@epitech.eu>
2020-02-05 15:24:55 +0100

    finished request creating an account from create account activity

--- client_mobile/app/src/main/java/com/example/client_mobile/Start.kt
+++ client_mobile/app/src/main/java/com/example/client_mobile/Start.kt
@@ -14,7 +14,6 @@ class Start : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start)
-        askForConnection()
 
         buttonLogin.setOnClickListener {
             val intent = Intent(this, Home::class.java)
@@ -25,34 +24,6 @@ class Start : AppCompatActivity() {
             val intent = Intent(this, createAccount::class.java)
             startActivity(intent)
         }
-    }
-
-    fun askForConnection() {
-
-
-        val client = OkHttpClient()
-        val formBody: RequestBody = FormBody.Builder()
-            .add("username", "test")
-            .add("password", "pwd")
-            .build()
-
-        val request: Request = Request.Builder()
-            .url("http://10.29.124.224:8080/createUser")
-            .post(formBody)
-            .build()
-
-        client.newCall(request).enqueue(object: Callback {
-            override fun onResponse(call: Call, response: Response) {
-                val body = response.body?.string()
-                println("response ouui oui oui")
-                println(body)
-
-            }
-            override fun onFailure(call: Call, e: IOException) {
-                println("Failed to execute request")
-                println(e)
-            }
-        })
     }
 
     class Account(val username: String, val password: String)
