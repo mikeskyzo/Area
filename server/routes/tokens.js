386e5462a9127ae2a153be754f4c952188b8c70c
Mike <mikael.gerard@epitech.eu>
2020-02-09 20:19:44 +0100

    ADD : Github service : First action -> push on a repository with the deletion if the area is delete

--- server/routes/tokens.js
+++ server/routes/tokens.js
@@ -1,30 +1,9 @@
 exports.newAuth = function (req, res)
 {
-	if (!req.body.access_token || !req.body.refresh_token || !req.body.expires_in) {
-		res.status(401);
-		res.json({
-			success : false,
-			message : 'Bad body'
-		});
-		return;
-	}
-
-	var json = new Object();
-	json.user_id = req.body.user_id;
-	json.created_the = Date.now();
-	json.access_token = req.body.access_token;
-	json.refresh_token = req.body.refresh_token;
-	json.expires_in = req.body.expires_in;
-
-	switch (req.params.app) {
-		case global.serviceDiscord:
-			json.service = global.serviceDiscord
-			break;
-		default:
-			global.responseError(res, 401, 'Application not found');
-			return;
-		};
-	global.saveInDb(global.CollectionToken, json, req, res, 'Token saved');
+	if (global.ActionTokenCheckMap.get(req.body.service))
+		global.ActionTokenCheckMap.get(req.body.service)(req, res);
+	else
+		global.responseError(res, 401, 'Service unknown')
 };
 
 exports.getServices = function (req, res)
@@ -40,20 +19,24 @@ exports.getServices = function (req, res)
 		}
 		var json = new Object();
 		json.Discord = false;
-		json.Reddit = false;
+		json.Github = false;
 		json.Imgur = false;
 		json.Steam = false;
 
 		var i = 0;
 		while (i < result.length) {
 			var tk = result[i];
-			if (tk.service == global.serviceDiscord)
+			if (!tk.service) {
+				i++;
+				continue;
+			}
+			if (tk.service == global.service.Discord)
 				json.Discord = true;
-			if (tk.service == global.serviceReddit)
-				json.Reddit = true;
-			if (tk.service == global.serviceImgur)
+			if (tk.service == global.service.Github)
+				json.Github = true;
+			if (tk.service == global.service.Imgur)
 				json.Imgur = true;
-			if (tk.service == global.serviceSteam)
+			if (tk.service == global.service.Steam)
 				json.Steam = true;
 			i++;
 		}
