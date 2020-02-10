c98c5674836c48b15bf7d5954b3192c180203124
Matthieu <desrues.matthieu@gmail.com>
2020-02-10 14:27:52 +0100

    [ADD] /client.apk route and /mobile route to access it

--- web-client/src/client.js
+++ web-client/src/client.js
@@ -27,5 +27,24 @@ app.get('/profil', function (req, res) {
     res.render('profil.ejs');
 });
 
+app.get('/mobile', function (req, res) {
+    res.render('mobile.ejs');
+});
+
+app.get('/client.apk', function (req, res) {
+    const path = require('path');
+    const fs = require('fs');
+
+    const filePath = "./public/app/release/";
+    const fileName = "app-release.apk";
+
+    fs.access(path.join(filePath, fileName), fs.F_OK, (err) => {
+        if (err)
+            console.error(err);
+        else
+            res.download(path.join(filePath, fileName));
+    });
+});
+
 app.listen(8081);
 app.on('listening', ()=>{});
