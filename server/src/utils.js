13c9421eac5f4e8de2f574e277547dcab323de60
Zargith <stanislas.hegron@gmail.com>
2020-02-12 14:06:01 +0100

    Working on using ping from trello's webhook

--- server/src/utils.js
+++ server/src/utils.js
@@ -29,18 +29,22 @@ global.ServiceTokenCheckMap.set(global.service.slack, Slack.CheckToken)
 // Action name
 global.Action = new Object();
 global.Action.github_new_push = 'github_new_push'
+global.Action.trello_new_update = 'trello_new_update'
 
 // Map Action creation Function
 global.ActionMap = new Map();
 global.ActionMap.set(global.Action.github_new_push, Github.createWebhookPushOnRepo)
+global.ActionMap.set(global.Action.trello_new_update, Trello.createNewWebhook)
 
 // Map of formating the result of the webhooks
 global.ActionFormatResultMap = new Map();
 global.ActionFormatResultMap.set(global.Action.github_new_push, Github.FormatWebhookPushOnRepo)
+global.ActionFormatResultMap.set(global.Action.trello_new_update, Trello.FormatWebhookUpdateModel)
 
 // Map for the deletion of the webhooks
 global.ActionDeleteWebhookMap = new Map();
 global.ActionDeleteWebhookMap.set(global.Action.github_new_push, Github.deleteWebhook)
+global.ActionDeleteWebhookMap.set(global.Action.trello_new_update, Trello.deleteWebhook)
 
 
 // Reaction name
