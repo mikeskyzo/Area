336d80ff6170b6a9c20a74ced7b23fcd83da4d90
Mike <mikael.gerard@epitech.eu>
2020-02-17 16:47:36 +0100

    ADD : c

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -18,10 +18,67 @@ router.get('/GetArea', function(req, res, next) {
 	utils.verifyToken(req, res, area.getAreas);
 });
 
-router.post('/UpdateArea', function(req, res, next) {
-	res.send('This route id deprecated');
-	// This will create a new webhook without delete the actual
-	// utils.verifyToken(req, res, area.updateArea);
+router.get('/getActionsReactions', function(req, res, next) {
+	var json = {
+		services : [
+			{
+				"name" : global.service.Github,
+				"actions" : [
+					{
+						"name" : global.Action.github_new_push,
+						"title" : "Repository push",
+						"description" : "Trigger when someone push on a repo",
+						"params" : [
+							{
+								"name" : "repository",
+								"description" : "Name of the repository"
+							},
+							{
+								"name" : "owner",
+								"description" : "Name of the owner of the repository"
+							}
+						]
+					},
+					{
+						"name" : global.Action.github_issue_event,
+						"title" : "Issue event",
+						"description" : "Trigger when a issue is update or created",
+						"params" : [
+							{
+								"name" : "repository",
+								"description" : "Name of the repository"
+							},
+							{
+								"name" : "owner",
+								"description" : "Name of the owner of the repository"
+							}
+						]
+					}
+				]
+			},
+			{
+				"name" : global.service.Slack,
+				"reactions" : [
+					{
+						"name" : global.Reaction.slack_send_message,
+						"title" : "Send a message",
+						"description" : "Send a message on a slack channel",
+						"params" : [
+							{
+								"name" : "channel_id",
+								"description" : "ID of the channel"
+							},
+							{
+								"name" : "message",
+								"description" : "message to send on the channel"
+							}
+						]
+					}
+				]
+			}
+		]
+	}
+	res.json(json);
 });
 
 router.delete('/DeleteArea', function(req, res, next) {
