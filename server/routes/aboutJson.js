95a927029b09b50f0635073c7ce050d80da6b9b3
Mike <mikael.gerard@epitech.eu>
2020-02-17 16:48:30 +0100

    ADD : route to get all actions and reactions possible

--- server/routes/aboutJson.js
+++ server/routes/aboutJson.js
@@ -38,3 +38,67 @@ exports.sendAbout = function(req, res) {
 		}
 	});
 };
+
+exports.getActionsReaction = function (req, res)
+{
+	var json = {
+		services : [
+			{
+				"name" : global.service.Github,
+				"actions" : [
+					{
+						"name" : global.Action.github_new_push,
+						"title" : "Repository push",
+						"description" : "Trigger when someone push on a repo",
+						"params" : [
+							{
+								"name" : "repository",
+								"description" : "Name of the repository"
+							},
+							{
+								"name" : "owner",
+								"description" : "Name of the owner of the repository"
+							}
+						]
+					},
+					{
+						"name" : global.Action.github_issue_event,
+						"title" : "Issue event",
+						"description" : "Trigger when a issue is update or created",
+						"params" : [
+							{
+								"name" : "repository",
+								"description" : "Name of the repository"
+							},
+							{
+								"name" : "owner",
+								"description" : "Name of the owner of the repository"
+							}
+						]
+					}
+				]
+			},
+			{
+				"name" : global.service.Slack,
+				"reactions" : [
+					{
+						"name" : global.Reaction.slack_send_message,
+						"title" : "Send a message",
+						"description" : "Send a message on a slack channel",
+						"params" : [
+							{
+								"name" : "channel_id",
+								"description" : "ID of the channel"
+							},
+							{
+								"name" : "message",
+								"description" : "message to send on the channel"
+							}
+						]
+					}
+				]
+			}
+		]
+	};
+	res.json(json);
+}
