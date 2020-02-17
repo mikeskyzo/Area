38f3bb25703cd7576690f67a6dcdb4e2b6ffeec6
Jean <jean.bourgarel@epitech.eu>
2020-02-17 16:20:49 +0100

    added recycler view

--- client_mobile/app/src/main/java/com/example/client_mobile/selectAction.kt
+++ client_mobile/app/src/main/java/com/example/client_mobile/selectAction.kt
@@ -1,8 +1,11 @@
 package com.example.client_mobile
 
 import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.epicture.MainAdapter
 import kotlinx.android.synthetic.main.activity_select_action.*
 
 class selectAction : AppCompatActivity() {
@@ -15,5 +18,9 @@ class selectAction : AppCompatActivity() {
             val intent = Intent(this, Home::class.java)
             startActivity(intent)
         }
+
+//        recyclerView_main.setBackgroundColor(Color.BLUE)
+        recyclerView_main.layoutManager = LinearLayoutManager(this)
+        recyclerView_main.adapter = MainAdapter()
     }
 }
