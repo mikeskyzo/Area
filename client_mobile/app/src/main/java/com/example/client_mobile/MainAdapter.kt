0a8a1b98eafd63f83ffc8d046ab64bebd27919c1
Jean <jean.bourgarel@epitech.eu>
2020-02-18 15:56:13 +0100

    can retrieve action's parameter to send to another recycler view

--- client_mobile/app/src/main/java/com/example/client_mobile/MainAdapter.kt
+++ client_mobile/app/src/main/java/com/example/client_mobile/MainAdapter.kt
@@ -1,10 +1,11 @@
 package com.example.epicture
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
-import com.example.client_mobile.Action
 import com.example.client_mobile.ActionReaction
 import com.example.client_mobile.R
 import kotlinx.android.synthetic.main.action_row.view.*
@@ -39,10 +40,15 @@ class MainAdapter(val actionReaction: ActionReaction): RecyclerView.Adapter<Cust
             holder.view.imageViewIcon.setImageResource(R.drawable.ic_discord)
         if (action.service == "Trello")
             holder.view.imageViewIcon.setImageResource(R.drawable.ic_trello)
+        holder.view.buttonAction.setOnClickListener {
+            Toast.makeText(holder.view.context, holder.view.buttonAction.text.toString(), Toast.LENGTH_SHORT).show()
+            for (element in action.params) {
+                println(element.name)
+            }
+        }
     }
 
 }
 
 class CustomViewHolder(val view: View): RecyclerView.ViewHolder(view) {
-
 }
