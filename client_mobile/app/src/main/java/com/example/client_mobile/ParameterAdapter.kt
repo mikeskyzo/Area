a8ae7a4b9e21e654068912fbab058f102f422288
Jean <jean.bourgarel@epitech.eu>
2020-02-18 18:26:44 +0100

    displays actions parameters in another recycler view

---
+++ client_mobile/app/src/main/java/com/example/client_mobile/ParameterAdapter.kt
@@ -0,0 +1,37 @@
+package com.example.epicture
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+import com.example.client_mobile.Action
+import com.example.client_mobile.ActionReaction
+import com.example.client_mobile.Param
+import com.example.client_mobile.R
+import kotlinx.android.synthetic.main.action_row.view.*
+import kotlinx.android.synthetic.main.parameter_row.view.*
+
+class ParameterAdapter(val params: Array<Param>): RecyclerView.Adapter<CustomViewHolderParam>() {
+
+    override fun getItemCount(): Int {
+        val nb = params.count()
+        return nb
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CustomViewHolderParam {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val cellForRow = layoutInflater.inflate(R.layout.parameter_row, parent, false)
+        return CustomViewHolderParam(cellForRow)
+    }
+
+    override fun onBindViewHolder(holder: CustomViewHolderParam, position: Int) {
+
+        val param = params.get(position)
+
+        holder.view.textViewParameter.setText(param.description)
+    }
+}
+
+class CustomViewHolderParam(val view: View): RecyclerView.ViewHolder(view) {
+}
