a81b984c7db748e89d03e415c651cea97c8552a8
Jean <jean.bourgarel@epitech.eu>
2020-02-22 15:46:28 +0100

    retrieved token in selectParameter

--- client_mobile/app/src/main/java/com/example/client_mobile/selectParameter.kt
+++ client_mobile/app/src/main/java/com/example/client_mobile/selectParameter.kt
@@ -20,19 +20,12 @@ class selectParameter : AppCompatActivity() {
         lateinit var action: Action
     }
 
-    inline fun <reified T> genericType() = object: TypeToken<T>() {}.type
-
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_select_parameter)
 
         if (intent.getStringExtra("token") != null)
             token = intent.getStringExtra("token")
-
-        if (intent.getSerializableExtra("action") != null)
-        println(token)
 
         imageButtonBack.setOnClickListener {
             val intent = Intent(this, selectAction::class.java)
@@ -46,8 +39,11 @@ class selectParameter : AppCompatActivity() {
         if (intent.getStringExtra("params") != null) {
             val paramsAsString = intent.getStringExtra("params")
             val listParam = Gson().fromJson(paramsAsString, Array<Param>::class.java)
-            recyclerView_param.adapter = ParameterAdapter(listParam)
+            recyclerView_param.adapter = ParameterAdapter(listParam, action.name)
         }
+        println("start")
+        println(action.name)
+        println(token)
     }
 
     fun getContext(): Context? {
