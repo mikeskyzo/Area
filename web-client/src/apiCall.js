d344163839c7d5f726ac985b30892ef775fc80b2
Benjamin <benjamin.piniac@epitech.eu>
2020-02-24 19:39:34 +0000

    auto connect wit cookie

--- web-client/src/apiCall.js
+++ web-client/src/apiCall.js
@@ -6,14 +6,6 @@ global.document = document;
 
 var $ = jQuery = require('jquery')(window);
 
-onComplete = function(msg) {
-    console.log('data ' + msg.data + ', response ' + msg.response.statusCode + ', request ' + msg.request.method);
-};
-
-onError = function(err) {
-    console.error(err);
-};
-
 exports.connectUser = function(username, password, server, req, res) {
     var url = server + '/connectUser';
 
@@ -47,7 +39,7 @@ exports.createUser = function(email, Uname, Pword, server, req, res) {
             res.redirect('/dashboard');
         },
         error : function (data, status, error) {
-            res.redirect('/test');
+            res.redirect('/error');
         }
     });
 };
