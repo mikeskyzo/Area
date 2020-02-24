c55e65a5c68cc6a22349730751069230f65533fc
Jean <jean.bourgarel@epitech.eu>
2020-02-24 19:01:26 +0100

    trying to send area

--- client_mobile/app/src/main/java/com/example/client_mobile/selectParameter.kt
+++ client_mobile/app/src/main/java/com/example/client_mobile/selectParameter.kt
@@ -3,13 +3,18 @@ package com.example.client_mobile
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.gson.Gson
 import kotlinx.android.synthetic.main.activity_select_action.imageButtonBack
 import kotlinx.android.synthetic.main.activity_select_parameter.*
 import kotlinx.android.synthetic.main.parameter_row.view.*
+import okhttp3.*
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.RequestBody.Companion.toRequestBody
+import okhttp3.ResponseBody.Companion.create
+import java.io.IOException
 
 
 class selectParameter : AppCompatActivity() {
@@ -46,6 +51,7 @@ class selectParameter : AppCompatActivity() {
                 recyclerView_param.adapter = ParameterAdapter(listParam, action.name)
             }
             if (intent.getSerializableExtra("reaction") != null) {
+                buttonCreateReaction.text = "Create area"
                 reaction = intent.getSerializableExtra("reaction") as Reaction
                 recyclerView_param.adapter = ParameterAdapter(listParam, reaction.name)
             }
@@ -65,15 +71,79 @@ class selectParameter : AppCompatActivity() {
                 intent.putExtra("action", action)
             }
             if (intent.getSerializableExtra("reaction") != null) {
+                Toast.makeText(getContext(), "send request", Toast.LENGTH_SHORT).show()
                 for (i in 0 until list.size) {
                     reaction.params[i].value = list[i]
                 }
                 intent.putExtra("reaction", reaction)
+                createArea()
+                println(action.name)
+                println(reaction.name)
             }
-            val intent = Intent(this, selectReaction::class.java)
+/*            val intent = Intent(this, selectReaction::class.java)
             intent.putExtra("token", token)
-            startActivity(intent)
+            startActivity(intent)*/
         }
+    }
+
+    fun formatRequest(): String {
+        val actionAsString: String = Gson().toJson(action)
+        val reactionAsString: String = Gson().toJson(reaction)
+        val jsonBody = "{\"action\":".plus(actionAsString).plus(",\"reaction\":").plus(reactionAsString).plus("}")
+        return jsonBody
+    }
+
+    fun createArea() {
+        val myBody = formatRequest().trimIndent()
+        val client = OkHttpClient()
+
+        val sex = myBody.toRequestBody("application/json; charset=utf-8".toMediaTypeOrNull())
+        val geh = "{\"action\" : { \"name\" : \"github_issue_event\"},\"reaction\" : { \"name\" : \"slack_send_message\"}}".toRequestBody("application/json; charset=utf-8".toMediaTypeOrNull())
+
+
+
+/*        val formBody: RequestBody = FormBody.Builder()
+            .add("action", Gson().toJson(action))
+            .add("reaction", Gson().toJson(reaction))
+            .build()*/
+
+        val request: Request = Request.Builder()
+            .url(Home.server_location.plus("/CreateArea"))
+            .header("Authorization", "token ".plus(token.toString()))
+            .header("Content-Type", "application/json")
+            .post(geh)
+            .build()
+
+        client.newCall(request).enqueue(object: Callback {
+            override fun onResponse(call: Call, response: Response) {
+                val body = response.body?.string()
+                println("RESP:")
+                println(body)
+                println(sex)
+                println("END")
+                if (body == "404") {
+//                    loadingPanel.visibility = View.GONE
+                    runOnUiThread {
+                        Toast.makeText(getContext(), "Error 404: server not found", Toast.LENGTH_SHORT).show()
+                    }
+                } else {
+                    runOnUiThread {
+                        val code = response.code
+//                        loadingPanel.visibility = View.GONE
+                        if (code >= 400) {
+                            Toast.makeText(getContext(), response.message, Toast.LENGTH_SHORT).show()
+                        } else {
+/*                            val intent = Intent(getContext(), Home::class.java)
+                            startActivity(intent)*/
+                        }
+                    }
+                }
+            }
+            override fun onFailure(call: Call, e: IOException) {
+                println("Failed to execute request")
+                println(e)
+            }
+        })
     }
 
     fun getContext(): Context? {
