bb189e19ac2f67293a329048eea583ff199dae9b
Marc-antoine <marc-antoine.wittling@epitech.eu>
2020-02-25 09:23:01 +0100

    no more error 400

--- server/services/twitch.js
+++ server/services/twitch.js
@@ -1,100 +1,99 @@
 const fetch = require("node-fetch");
-let myHeaders = new Headers();
 const client_id = '5cfjsk5flx3vkouomr7a26y7usrxmz';
 
-myHeaders.append("client-id", client_id.length.toString());
-
-exports.Twitch_UserId = async function(login)
+async function Twitch_UserId (login)
 {
-    let url = 'https://api.twitch.tv/helix/users?login=';
-    let myInit = { method: 'GET',
-        headers: myHeaders,
-    };
+    var url = 'https://api.twitch.tv/helix/users?login=';
     url += login;
-    let object = await fetch(url,myInit).json();
-    if(object.hasOwnProperty('data')){
-        return(object.data[0]);
+    var object = await fetch(url,{
+        'method': 'GET',
+        'headers' : {'Client-ID' : client_id}
+    });
+    var json = await object.json()
+    if(json.hasOwnProperty('data')){
+        return(json.data[0].id);
     }
     return(84);
 
 };
 
-exports.Twitch_Create_Webhook_NewSubscriber = async function(login, req, res, json)
+exports.Twitch_Create_Webhook_NewSubscriber = async function(req, res, login)
 {
-    let user_id = Twitch_UserId(login);
-    let myInit = { method: 'POST',
-        headers: myHeaders,
-    };
-
-    if (user_id == 84) {
-        global.responseError(res, 401, 'error');
-        return (0);
+    var user_id = await Twitch_UserId(login);
+    if (user_id === 84) {
+        global.responseError(res, 401, 'error 84, it is nut the good stuffffff uwu');
+        return ;
     }
 
-    let url = `https://api.twitch.tv/helix/webhooks/hub?redirect_url=${global.url/sex}&hub.topic=https://api.twitch.tv/helix/users/follows?to_id=${user_id}&hub.mode=subscribe
-    &hub.callback=http://localhost:8080/`;
-   let resp = await fetch(url, myInit);
+    let url = `https://api.twitch.tv/helix/webhooks/hub?redirect_url=${global.url}/sex&hub.topic=https://api.twitch.tv/helix/users/follows?to_id=${user_id}&hub.mode=subscribe&hub.callback=http://localhost:8080/`;
+   let resp = await fetch(url, {
+       'method': 'POST',
+       'headers' : {'Client-ID' : client_id}
+   });
+   console.log(resp.status);
+   console.log(resp.statusText);
+   console.log(url)
    if (resp.status == 202)
        next(req, res, json);
     else
-        global.responseError(res, 401, 'error');
+        global.responseError(res, 401, 'ewwow it not wok owo');
 };
 
-exports.Twitch_Delete_Webhook_NewSubscriber = async function(login)
+exports.Twitch_Delete_Webhook_NewSubscriber = async function(req, res, json, login)
+{
+    var user_id = await Twitch_UserId(login);
+    if (user_id === 84) {
+        global.responseError(res, 401, 'error 84, it is nut the good stuffffff uwu');
+        return ;
+    }
+
+    let url = `https://api.twitch.tv/helix/webhooks/hub?redirect_url=${global.url}/sex&hub.topic=https://api.twitch.tv/helix/users/follows?to_id=${user_id}&hub.mode=unsubscribe&hub.callback=http://localhost:8080/`;
+    let resp = await fetch(url, {
+        'method': 'POST',
+        'headers' : {'Client-ID' : client_id}
+    });
+    console.log(resp.status);
+    console.log(resp.statusText);
+    console.log(url)
+    if (resp.status == 202)
+        next(req, res, json);
+    else
+        global.responseError(res, 401, 'ewwow it not wok owo');
+};
+
+exports.Twitch_Create_Webhook_StreamChangeState = async function(req, res, json, login)
 {
     let user_id = Twitch_UserId(login);
-    let myInit = { method: 'POST',
-        headers: myHeaders,
-    };
 
     if (user_id == 84) {
         global.responseError(res, 401, 'error');
         return (0);
     }
 
-    let url = `https://api.twitch.tv/helix/webhooks/hub?redirect_url=${global.url/sex}&hub.topic=https://api.twitch.tv/helix/users/follows?to_id=${user_id}&hub.mode=unsubscribe
-    &hub.callback=http://localhost:8080/`;
-    let resp = await fetch(url, myInit);
+    let url = `https://api.twitch.tv/helix/webhooks/hub?redirect_url=${global.url}/sex&hub.topic=https://api.twitch.tv/helix/streams?user_id=${user_id}&hub.mode=subscribe&hub.callback=http://localhost:8080/`;
+    let resp = await fetch(url,  {
+        'method': 'POST',
+        'headers' : {'client_id' : client_id}
+    });
     if (resp.status == 202)
         next(req, res, json);
     else
         global.responseError(res, 401, 'error');
 };
 
-exports.Twitch_Create_Webhook_StreamChangeState = async function(login)
+exports.Twitch_Delete_Webhook_StreamChangeState = async function(req, res, json, login)
 {
     let user_id = Twitch_UserId(login);
-    let myInit = { method: 'POST',
-        headers: myHeaders,
-    };
-
     if (user_id == 84) {
         global.responseError(res, 401, 'error');
         return (0);
     }
 
-    let url = `https://api.twitch.tv/helix/webhooks/hub?redirect_url=${global.url/sex}&hub.topic=https://api.twitch.tv/helix/streams?user_id=${user_id}&hub.mode=subscribe&hub.callback=http://localhost:8080/`;
-    let resp = await fetch(url, myInit);
-    if (resp.status == 202)
-        next(req, res, json);
-    else
-        global.responseError(res, 401, 'error');
-};
-
-exports.Twitch_Delete_Webhook_StreamChangeState = async function(login)
-{
-    let user_id = Twitch_UserId(login);
-    let myInit = { method: 'POST',
-        headers: myHeaders,
-    };
-
-    if (user_id == 84) {
-        global.responseError(res, 401, 'error');
-        return (0);
-    }
-
-    let url = `https://api.twitch.tv/helix/webhooks/hub?redirect_url=${global.url/sex}&hub.topic=https://api.twitch.tv/helix/streams?user_id=${user_id}&hub.mode=unsubscribe&hub.callback=http://localhost:8080/`;
-    let resp = await fetch(url, myInit);
+    let url = `https://api.twitch.tv/helix/webhooks/hub?redirect_url=${global.url}/sex&hub.topic=https://api.twitch.tv/helix/streams?user_id=${user_id}&hub.mode=unsubscribe&hub.callback=http://localhost:8080/`;
+    let resp = await fetch(url,  {
+        'method': 'POST',
+        'headers' : {'client_id' : client_id}
+    });
     if (resp.status == 202)
         next(req, res, json);
     else
