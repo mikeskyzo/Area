7fdd8c49cdbace1300a01c2fdea33977f4b71c21
MinMatth-Magi <matthieu.desrues@epitech.eu>
2020-02-25 17:46:03 +0100

    [ADD] Now getting Reddit access token

--- web-client/services/RedditApi.js
+++ web-client/services/RedditApi.js
@@ -1,56 +1,86 @@
-import axios from 'axios';
+var axios = require('axios');
 
-// General settings
+	/* General settings */
 const generalSettings = {
-		// Api
+	// Api
 	redditApi: `https://www.reddit.com/api/v1/`,
 	redditAuthApi: `https://oauth.reddit.com/api/v1`,
 
-		// App related
+	// App related
 	clientId: 'xxa4cp-hsWE_iA',
 	clientSecret: '466F9UWdI-Eh1iz7AhN8zNyszE8',
 	redirectUri: 'http://localhost:8081/authorizations/reddit',
 
-		// Session related
+	// Session related
 	code: '', // To get in redirectUri req after calling GENERATE CODE
 	authorizationToken: '' // to get in res after calling GET AUTHORIZATION TOKEN
 };
 
-	// Initialize axios
-const RedditApi = axios.create({baseURL: generalSettings.redditApi});
-const RedditAuthApi = axios.create({baseURL: generalSettings.redditAuthApi});
+	/* Initialize axios */
+const RedditApi = axios.create({
+	baseURL: generalSettings.redditApi,
+	crossDomain: true
+});
+const RedditAuthApi = axios.create({
+	baseURL: generalSettings.redditAuthApi,
+	crossDomain: true
+});
 
-	// Creating Api methods
-//export default {
-
-	// Authorization
-const authorize = function () {
-	const responseType = 'code';
-	return RedditApi.get(`authorize?client_id=${generalSettings.clientId}&response_type=${responseType}`)
-};
-
-	// Generate code
-const generateCode = function () {
+	/* Generate code */
+function generateCodeUrl () {
 	const responseType = 'code';
 	const scope = 'edit identity flair history modconfig modflair modlog modposts modwiki mysubreddits privatemessages read report save submit subscribe vote wikiedit wikiread';
 	const duration = 'permanent';
 	const state = 'NONE';
 
-	console.log("Oui");
-	return RedditApi.get(`authorize?client_id=${generalSettings.clientId}&redirect_uri=${generalSettings.redirectUri}&scope=${scope}&response_type=${responseType}&duration=${duration}&state=${state}`)
-};
+	return `authorize` +
+		`?client_id=${generalSettings.clientId}` +
+		`&redirect_uri=${generalSettings.redirectUri}` +
+		`&scope=${scope}` +
+		`&response_type=${responseType}` +
+		`&duration=${duration}` +
+		`&state=${state}`
+}
 
-	// Get access token (require to get code first)
-const getAccessToken = function () {
-	// Need Basic Auth =>
-	//   username: clientId
-	//   password: clientSecret
-	const grantType = 'authorization_code';
-	return RedditApi.post(`access_token?grant_type=${grantType}&code=${generalSettings.code}&redirect_uri=${generalSettings.redirectUri}`)
-};
 
-	// Get profile
-const getProfile = function () {
-	// Need Header => Authorization: `bearer ${generalSettings.authorizationCode}`
-	return RedditAuthApi.get(me)
+module.exports = {
+
+	generalSettings: generalSettings,
+
+
+/* Creating Api methods */
+
+	/* Authorization */
+	authorize:
+		function () {
+			const responseType = 'code';
+			return RedditApi.get(`authorize?client_id=${generalSettings.clientId}&response_type=${responseType}`)
+		},
+
+	/* Get access token (require to get code first) */
+	getAccessToken: async function () {
+		// Need Basic Auth =>
+		//   username: clientId
+		//   password: clientSecret
+		const grantType = 'authorization_code';
+		return await RedditApi.post(`access_token` +
+			`?grant_type=${grantType}` +
+			`&code=${generalSettings.code}` +
+			`&redirect_uri=${generalSettings.redirectUri}`,
+			{},
+			{
+				auth: {
+					username: generalSettings.clientId,
+					password: generalSettings.clientSecret
+				}
+			}
+		)
+	},
+
+	/* Get profile */
+	getProfile: function () {
+		// Need Header => Authorization: `bearer ${generalSettings.authorizationCode}`
+		return RedditAuthApi.get(`me`)
+	}
+
 };
