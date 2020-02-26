6f5d61ad909e137b1a78e4cdc6a184b0eba30905
Marc-antoine <marc-antoine.wittling@epitech.eu>
2020-02-26 13:50:54 +0100

    test

--- server/services/twitch.js
+++ server/services/twitch.js
@@ -46,17 +46,17 @@ exports.Twitch_Delete_Webhook_NewSubscriber = async function(req, res, json, log
         global.responseError(res, 401, 'error 84, it is nut the good stuffffff uwu');
         return ;
     }
-
-    let url = `https://api.twitch.tv/helix/webhooks/hub?redirect_url=${global.url}/sex&hub.topic=https://api.twitch.tv/helix/users/follows?to_id=${user_id}&hub.mode=unsubscribe&hub.callback=http://localhost:8080/`;
+    console.log(global.url);
+    let url = `https://api.twitch.tv/helix/webhooks/hub?hub.topic=https://api.twitch.tv/helix/users/follows?to_id=${user_id}&hub.mode=unsubscribe&hub.callback=${global.url}/caca&hub.lease_seconds=86400`;
     let resp = await fetch(url, {
         'method': 'POST',
         'headers' : {'Client-ID' : client_id}
     });
-    console.log(resp.status);
-    console.log(resp.statusText);
-    console.log(url)
-    if (resp.status == 202)
-        next(req, res, json);
+
+    if (resp.status == 202) {
+        return (0);
+        //  next(req, res, json);
+    }
     else
         global.responseError(res, 401, 'ewwow it not wok owo');
 };
