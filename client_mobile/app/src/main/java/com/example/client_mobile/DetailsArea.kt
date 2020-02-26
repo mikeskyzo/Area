35aa890dd599ccefaf251aaf4ce2217767a5ad01
Jean <jean.bourgarel@epitech.eu>
2020-02-26 14:58:04 +0100

    created DetailsArea class

---
+++ client_mobile/app/src/main/java/com/example/client_mobile/DetailsArea.kt
@@ -0,0 +1,10 @@
+package com.example.client_mobile
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class DetailsArea : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
