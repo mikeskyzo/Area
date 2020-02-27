cd1ea83af6b1b8519e875c5683ae90448fdf230e
MinMatth-Magi <matthieu.desrues@epitech.eu>
2020-02-27 13:48:14 +0100

    [ADD] Trello access_token sent to server, still, there is an error

--- web-client/services/TrelloApi.js
+++ web-client/services/TrelloApi.js
@@ -50,17 +50,27 @@ module.exports = {
 	},
 
 	getAccessToken: function (req, res) {
+		console.log('Starting function...');
 		const query = url.parse(req.url, true).query;
 		const token = query.oauth_token;
 		const tokenSecret = oauthSecrets[token];
 		const verifier = query.oauth_verifier;
 
-		oauth.getOAuthAccessToken(token, tokenSecret, verifier, function(error, accessToken, accessTokenSecret, results){
-			oauth.getProtectedResource("https://api.trello.com/1/members/me", "GET", accessToken, accessTokenSecret, function(error, data, response) {
-				generalSettings.authorizationToken = accessToken;
-				generalSettings.secretAuthorizationToken = accessTokenSecret;
-				console.log(generalSettings);
-			});
+		console.log('Starting Oauth call...');
+		oauth.getOAuthAccessToken(
+			token, tokenSecret, verifier,
+			function(error, accessToken, accessTokenSecret, results) {
+				console.log('Starting to get access token...');
+				oauth.getProtectedResource(
+					"https://api.trello.com/1/members/me",
+					"GET", accessToken, accessTokenSecret,
+					function(error, data, response) {
+						console.log('Starting to get secret...');
+						generalSettings.authorizationToken = accessToken;
+						generalSettings.secretAuthorizationToken = accessTokenSecret;
+						res.redirect('/authorizations/trello/post-steps');
+					}
+				)
 		});
 	}
 
