ac7b16ef13a696e3f5ff25b207736653a0a55583
MinMatth-Magi <matthieu.desrues@epitech.eu>
2020-02-28 12:30:15 +0100

    [ADD] Logger implementation (allow to log depending on a detail level. note: could be better with binary masks)

--- web-client/src/routes/Authorizations/redditAuthorizationRoutes.js
+++ web-client/src/routes/Authorizations/redditAuthorizationRoutes.js
@@ -1,15 +1,17 @@
-const RedditApi = require('../../../services/RedditApi');
-const ServerApi = require('../../../services/ApplicationServer');
+const RedditApi = require('../../services/RedditApi');
+const ServerApi = require('../../services/ApplicationServer');
 
 module.exports = {
 
 	enableRedditAuthorizationRoute: async function (app) {
 		app.get('/authorizations/reddit', async function (req, res) {
-			console.log('\n   ====== Reddit authorization ====== \n');
+
+			app.logger.log(1,'\n   ====== Reddit authorization ====== \n');
 			RedditApi.generalSettings.code = req.query.code;
 			const newres = await RedditApi.getAccessToken();
 			RedditApi.generalSettings.authorizationToken = newres.data.access_token;
 			ServerApi.setRedditAccessToken(req, res, RedditApi.generalSettings.authorizationToken);
+
 		});
 	}
 
