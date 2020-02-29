9f28301bfe9373ffc992aa8ec5a6b59a9e921e73
Jean <jean.bourgarel@epitech.eu>
2020-02-29 20:01:46 +0100

    added help view activity

--- client_mobile/app/src/main/java/com/example/client_mobile/Help.kt
+++ client_mobile/app/src/main/java/com/example/client_mobile/Help.kt
@@ -1,12 +1,18 @@
 package com.example.client_mobile
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_help.*
 
 class Help : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_help)
+        imageButtonBack.setOnClickListener {
+            val intent = Intent(this, Home::class.java)
+            startActivity(intent)
+        }
     }
 }
