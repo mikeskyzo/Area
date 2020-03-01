790f4803f73fd5f94306d67926e02c6cdce2ad85
Mike <mikael.gerard@epitech.eu>
2020-03-01 20:48:25 +0100

    ADD : mail service

---
+++ server/services/Mail.js
@@ -0,0 +1,67 @@
+var nodemailer = require('nodemailer');
+
+// mailer.extend(global.app, {
+// 	from: 'areacoon@gmail.com',
+// 	host: 'smtp.gmail.com', // hostname
+// 	secureConnection: true, // use SSL
+// 	port: 465, // port for secure SMTP
+// 	transportMethod: 'SMTP', // default is SMTP. Accepts anything that nodemailer accepts
+// 	auth: {
+// 	  user: 'areacoon@gmail.com',
+// 	  pass: 'Areacoon123'
+// 	}
+// });
+
+exports.is_service_active = async function (user_id)
+{
+	return true;
+}
+
+exports.send_message = async function (area, res)
+{
+	let email = global.getParam(area.reaction.params, 'email');
+	let message = global.getParam(area.reaction.params, 'message');
+
+	if (!email || !message) {
+		global.responseError(res, 401, 'Missing email or a message')
+	  	return;
+  	}
+
+	var transporter = nodemailer.createTransport({
+		host: 'smtp.gmail.com',
+		port: 587,
+		secure: false,
+		requireTLS: true,
+		auth: {
+			user: 'areacoon@gmail.com',
+			pass: 'Areacoon123'
+		}
+	});
+
+    var mailOptions = {
+        from: 'gerard.reat@gmail.com',
+        to : email,
+        subject: 'Areacoon',
+        text: message
+    };
+
+    transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+        	console.log(error);
+			res.send();
+        } else {
+			res.send();
+			console.log('Email sent: ' + info.response);
+        }
+    });
+}
+
+exports.send_message_check_args = function(json)
+{
+    if (!global.getParam(json.reaction.params, 'email'))
+		return 'Missing a email';
+    else if (!global.getParam(json.reaction.params, 'message'))
+		return 'Missing a message to send';
+    else
+		return null;
+}
