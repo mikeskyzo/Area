67b2fc43698e1bc51fb9688e85521613fd6c3500
MinMatth-Magi <matthieu.desrues@epitech.eu>
2020-03-01 21:58:13 +0100

    [CLEAN] Removing Apis from web-client

--- web-client/src/controllers/RedditOauthController.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-	var authenticateReddit = document.getElementById('authenticate-reddit');
-
-	console.log("Adding event listener");
-	authenticateReddit.addEventListener('href', generateCodeUrl);
-});
