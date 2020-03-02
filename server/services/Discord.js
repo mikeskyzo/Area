f179d083dafd93ea921e8ccd92a4e5cd71db6631
Mike <mikael.gerard@epitech.eu>
2020-03-02 18:57:08 +0100

    REMOVE + ADD : remove debug + add a action github

--- server/services/Discord.js
+++ server/services/Discord.js
@@ -56,26 +56,6 @@ exports.redirect_auth = async function (req, json)
 	})
 }
 
-exports.check_token = async function (req, res)
-{
-	if (!req.body.webhook_id || !req.body.webhook_token) {
-		global.responseError(res, 401, 'Need a access webhook_token and a webhook_id for Discord');
-		return;
-    }
-    let json = {
-        user_id : req.body.user_id,
-        service : global.Services.Discord,
-        webhook_id : req.body.webhook_id,
-        webhook_token : req.body.webhook_token
-    };
-    let url = 'https://discordapp.com/api/webhooks/' + req.body.webhook_id + '/' + req.body.webhook_token;
-    let response = await (await fetch(url, {}));
-    if (response.status != 200)
-        global.responseError(res, 401, 'Webhook not valid : ' + response.statusText);
-    else
-		global.saveInDb(global.CollectionToken, json, res, "Webhook Discord saved");
-}
-
 exports.send_message = async function (area, res)
 {
 	var token = await global.findInDbAsync(global.CollectionToken, {user_id : area.user_id, service : global.Services.Discord});
--- server/services/Github.js
+++ server/services/Github.js
@@ -45,6 +45,11 @@ exports.createWebhookPushOnRepo = function (res, json, next)
 	createWebhook('push', res, json, next);
 }
 
+exports.createWebhookRepoPublic = function (res, json, next)
+{
+	createWebhook('public', res, json, next);
+}
+
 async function createWebhook(event, res, json)
 {
 	let username = global.getParam(json.action.params, 'username');
@@ -137,16 +142,7 @@ exports.FormatWebhookIssueEvent = function (req, res, area, next)
 		res.send();
 		return;
 	}
-// 	let message = area.reaction.message;
-// 	if (message) {
-// 		if (message.includes('{event}'))
-// 			message = message.replace('{event}', req.body.action)
-// 		if (message.includes('{username}') && req.body.sender && req.body.sender.login)
-// 			message = message.replace('{username}', req.body.sender.login)
-// 		if (message.includes('{repository_name}') && req.body.repository && req.body.repository.name)
-// 			message = message.replace('{repository_name}', req.body.repository.name)
-// 	}
-// 	area.reaction.message = message
+
 	next(area, res);
 }
 
@@ -156,105 +152,14 @@ exports.FormatWebhookPushOnRepo = function (req, res, area, next)
 		res.send();
 		return;
 	}
-	// if (area.reaction.message) {
-	// 	if (area.reaction.message.includes('{name}') && req.body.pusher && req.body.pusher.name)
-	// 		area.reaction.message = area.reaction.message.replace('{name}', req.body.pusher.name)
-	// 	if (area.reaction.message.includes('{repository_name}') && req.body.repository && req.body.repository.name)
-	// 		area.reaction.message = area.reaction.message.replace('{repository_name}', req.body.repository.name)
-	// }
 	next(area, res);
 }
 
-exports.check_token = async function (req, res)
+exports.FormatWebhookRepoPublic = function (req, res, area, next)
 {
-	if (!req.body.access_token) {
-		global.responseError(res, 401, 'Need a access token');
-		return;
-	}
-	var token = await global.findInDbAsync(global.CollectionToken, {user_id : req.body.user_id, service : req.body.service})
-	if (token)
-		global.deleteInDbAsync(global.CollectionToken, {user_id : req.body.user_id, service : req.body.service});
-	fetch('https://api.github.com/user', {
-		'method': 'GET',
-		'headers' : {'Authorization' : 'token ' + req.body.access_token},
-	})
-	.then(function (response) {
-		if (response.status == 200) {
-			var json = {
-				user_id : req.body.user_id,
-				service : global.Services.Github,
-				access_token : req.body.access_token
-			}
-			global.saveInDb(global.CollectionToken, json, res, 'Token saved');
-			return;
-		}
-		global.responseError(res, 401, 'Token not valid : ' + response.statusText);
-	})
-	.catch(function (error) {
-		global.responseError(res, 500, error)
-	});
-}
-
-exports.create_board_check_args = function(res, json)
-{
-    if (!global.getParam(json.reaction.params, 'owner'))
-        global.responseError(res, 401, 'Missing the owner')
-    else if (!global.getParam(json.reaction.params, 'repository'))
-       global.responseError(res, 401, 'Missing the repository')
-   else if (!global.getParam(json.reaction.params, 'title'))
-       global.responseError(res, 401, 'Missing the title')
-   else if (!global.getParam(json.reaction.params, 'body'))
-       global.responseError(res, 401, 'Missing the body')
-    else {
-		// #### TODO : check if le project exist and we have the right to create a project on it
-        global.saveAREA(res, json);
-    }
-}
-
-exports.create_board = async function (area, res)
-{
-	let repository = global.getParam(area.reaction.params, 'repository');
-	let owner = global.getParam(area.reaction.params, 'owner');
-	let bodyGit = global.getParam(area.reaction.params, 'body');
-	let title = global.getParam(area.reaction.params, 'title');
-
-	if (!owner || !repository || !title || !bodyGit) {
-		global.responseError(res, 401, 'Missing something');
-		return;
-	}
-	var token = await global.findInDbAsync(global.CollectionToken, {user_id : area.user_id, service : global.Services.Github});
-	if (!token || !token.access_token) {
-		global.responseError(res, 401, 'No access token provide');
-		return;
-	}
-	var url =  'https://api.github.com/repos/' + owner + '/' + repository + '/projects';
-	console.log(url)
-	const body = {
-		"name" : title,
-		"body" : bodyGit
-	}
-
-	fetch(url, {
-		'method': 'POST',
-		'headers' : {'Authorization' : 'token ' + token.access_token,
-						'Accept' : 'application/vnd.github.inertia-preview+json',
-						'Content-Type' : 'application/x-www-form-urlencoded'
-					},
-		'body' : JSON.stringify(body),
-	})
-	.then(function (response) {
-		if (response.status >= 300) {
-			return response.json();
-		}
+	if (req.body.zen) {
 		res.send();
-		return ;
-	})
-	.then(function (resjson){
-		if (resjson)
-			console.error(resjson);
-		res.send();
-	})
-	.catch(function (error) {
-		global.responseError(res, 500, 'err : ' + error)
-	});
+		return;
+	}
+	next(area, res);
 }
