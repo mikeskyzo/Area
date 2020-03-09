d542947638857a0349e363f877c4a8bb73627641
Mike <mikael.gerard@epitech.eu>
2020-03-09 15:36:32 +0100

    CORRECT TWITCH : correct the deletation of webhooks

--- server/services/Twitch.js
+++ server/services/Twitch.js
@@ -47,7 +47,7 @@ exports.Twitch_Delete_Webhook_NewSubscriber = async function(area, req, res)
     var user_id = await Twitch_UserId(global.getParam(area.action.params, 'login'));
     if (user_id === -1)
         return 'Can\'t find the user'
-    let url = `https://api.twitch.tv/helix/webhooks/hub?hub.topic=https://api.twitch.tv/helix/users/follows?to_id=${user_id}&hub.mode=unsubscribe&hub.callback=${global.url}/webhooks/${area.id}&hub.lease_seconds=86400&hub.secret=qj183vwtldxe1k62knihlw0i5cti70`;
+    let url = `https://api.twitch.tv/helix/webhooks/hub?hub.topic=https://api.twitch.tv/helix/users/follows?to_id=${user_id}&hub.mode=unsubscribe&hub.callback=${global.url}/webhooks/${area.area_id}&hub.lease_seconds=86400&hub.secret=qj183vwtldxe1k62knihlw0i5cti70`;
     let resp = await fetch(url, {
         'method': 'POST',
         'headers' : {'Client-ID' : process.env.TWITCH_ID}
