e2d4d066919d788625e039c4a373aa3a62a1f940
Jean <jean.bourgarel@epitech.eu>
2020-03-17 19:00:30 +0100

    added instrumental tests for mobile

--- client_mobile/app/src/androidTest/java/com/example/client_mobile/ExampleInstrumentedTest.kt
+++ client_mobile/app/src/androidTest/java/com/example/client_mobile/ExampleInstrumentedTest.kt
@@ -1,15 +1,13 @@
 package com.example.client_mobile
 
-import android.content.Context
 import androidx.test.InstrumentationRegistry
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions.closeSoftKeyboard
-import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.intent.rule.IntentsTestRule
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.runner.AndroidJUnit4
-import androidx.test.rule.*
 import org.junit.Assert.assertEquals
 import org.junit.Rule
 import org.junit.Test
@@ -26,26 +24,36 @@ import org.junit.runner.RunWith
 class ExampleInstrumentedTest {
 
     @get:Rule
-    var activityRule: ActivityTestRule<Start>
-            = ActivityTestRule(Start::class.java)
+    var activityRule: IntentsTestRule<Start>
+            = IntentsTestRule(Start::class.java)
 
     @Test
     fun useAppContext() {
-        // Context of the app under test.
         val appContext = InstrumentationRegistry.getTargetContext()
         assertEquals("com.example.client_mobile", appContext.packageName)
     }
 
     @Test
-    fun testStart() {
-        // Type text and then press the button.
-        onView(withId(R.id.editTextUsername)).perform(typeText("Caca"), closeSoftKeyboard())
-        onView(withId(R.id.editTextUsername)).check(matches(withText("Caca")))
+    fun testSetUsername() {
+        onView(withId(R.id.editTextUsername)).perform(typeText("Username"), closeSoftKeyboard())
+        onView(withId(R.id.editTextUsername)).check(matches(withText("Username")))
     }
 
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun testSetPassword() {
+        onView(withId(R.id.editTextPassword)).perform(typeText("Password"), closeSoftKeyboard())
+        onView(withId(R.id.editTextPassword)).check(matches(withText("Password")))
+    }
+
+    @Test
+    fun testDefaultServerLocation() {
+        onView(withId(R.id.editTextServerLocation)).check(matches(withText("https://areacoon-api.eu.ngrok.io")))
+
+    }
+
+    @Test
+    fun test() {
+        assertEquals(2 + 2, 4)
     }
 }
 
